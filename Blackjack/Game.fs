abafeae0b3b711df50b89a8a2fc9c5adc55bb14c
adacola <adacola255@gmail.com>
2015-12-07 16:29:47 +0900

    ディーラーの行動を実装。ゲーム自体の関数を実装中。

---
+++ Blackjack/Game.fs
@@ -0,0 +1,75 @@
+﻿namespace Adacola.Blackjack
+
+/// ゲームの状態
+type GameState = {
+    /// 賭金
+    Bet : int
+    /// プレイヤーの手
+    PlayerHands : Card list
+    /// ディーラーの公開されている手
+    DealerHands : Card list
+}
+
+/// プレイヤーが使用できる行動
+type PlayerAction = Hit | Stand
+
+/// ディーラーの行動方法
+[<RequireQualifiedAccess>]
+type DealerStrategy = StandOnAll17 | HitOnSoft17
+
+/// ゲームの勝敗。プレイヤー目線での結果。
+type GameResult =  Lose | Draw | Win of isBlackjack : bool
+
+/// ゲームの最終的な状態
+type GameConclusiveState = {
+    /// ゲームの勝敗
+    Result : GameResult
+    /// プレイヤーの収支。プレイヤーの負の場合は賭金分のマイナス、引き分けの場合は0、プレイヤーの勝ちの場合は払い戻し額
+    Gain : int
+    /// ゲーム終了時の状態
+    State : GameState
+}
+
+type GameRule = {
+    /// ディーラーの行動方法
+    DealerStrategy : DealerStrategy
+    /// プレイヤーがBlackjackで勝利した場合の払い戻し率。1.5または1.2のケースが多いです。
+    PayRatioWinningBlackjack : float
+    /// デッキのうちどの程度ゲームに使用するか
+    Penetration : float
+}
+
+module Game =
+    open Adacola.Blackjack
+    open Basis.Core
+    open System
+
+    // ディーラーが使用できる行動
+    [<RequireQualifiedAccess>]
+    type internal DealerAction = Hit | Stand
+
+    let internal dealerStrategyStandOnAll17 = function
+        | Busted, _ -> failwith "Bustedでは何もできない"
+        | x, _ when x < Score 17 -> DealerAction.Hit
+        | _ -> DealerAction.Stand
+
+    let internal dealerStrategyHitOnSoft17 = function
+        | Busted, _ -> failwith "Bustedでは何もできない"
+        | Score 17, true -> DealerAction.Hit
+        | x, _ when x < Score 17 -> DealerAction.Hit
+        | _ -> DealerAction.Stand
+
+    let validateRule { PayRatioWinningBlackjack = payRatio; Penetration = penetration } =
+        if payRatio < 1.0 then Failure(ArgumentException(sprintf "PayRatioWinnerBlackjack(%f)は1以上である必要があります" payRatio))
+        elif penetration < 0.0 || 1.0 < penetration then Failure(ArgumentException(sprintf "Penetration(%f)は0～1の間である必要があります" penetration))
+        else Success ()
+
+    let play rule (playerStrategy : GameState -> PlayerAction) bet =
+        rule |> validateRule |> Result.iterFailure raise
+        let dealerStrategyFunc =
+            List.map Card.toNumber >> Score.calculate
+            >> match rule.DealerStrategy with
+                | DealerStrategy.StandOnAll17 -> dealerStrategyStandOnAll17
+                | DealerStrategy.HitOnSoft17 -> dealerStrategyHitOnSoft17
+        // TODO
+        System.NotImplementedException() |> raise
---
+++ BlackjackTest/GameTest.fs
@@ -0,0 +1,90 @@
+﻿namespace Adacola.Blackjack.Test
+
+open FsUnit
+open FsCheck
+open NUnit.Framework
+
+[<TestFixture>]
+module Game =
+    open Adacola.Blackjack
+    open Basis.Core
+
+    let getScoreGen withinBlackjack minScore maxScore = gen {
+        let scores = [for i in minScore .. maxScore -> Score i]
+        let patterns = if withinBlackjack then Blackjack::scores else scores
+        let! score = Gen.elements patterns
+        let! isSoft = Arb.generate<bool>
+        return score, isSoft
+    }
+
+    [<Test>]
+    let ``dealerStrategyStandOnAll17はScore(16)以下の場合Hitすること`` () =
+        Prop.forAll (getScoreGen false 1 16 |> Arb.fromGen) (Game.dealerStrategyStandOnAll17 >> (=) Game.DealerAction.Hit)
+        |> Check.QuickThrowOnFailure
+
+    [<Test>]
+    let ``dealerStrategyStandOnAll17はScore(17)以上の場合Standすること`` () =
+        Prop.forAll (getScoreGen true 17 21 |> Arb.fromGen) (Game.dealerStrategyStandOnAll17 >> (=) Game.DealerAction.Stand)
+        |> Check.QuickThrowOnFailure
+
+    [<Test>]
+    let ``dealerStrategyStandOnAll17はBustedの場合例外が発生すること`` () =
+        fun () -> Game.dealerStrategyStandOnAll17 (Busted, false) |> ignore
+        |> should throw typeof<exn>
+        fun () -> Game.dealerStrategyStandOnAll17 (Busted, true) |> ignore
+        |> should throw typeof<exn>
+
+    [<Test>]
+    let ``dealerStrategyHitOnSoft17はScore(16)以下の場合Hitすること`` () =
+        Prop.forAll (getScoreGen false 1 16 |> Arb.fromGen) (Game.dealerStrategyHitOnSoft17 >> (=) Game.DealerAction.Hit)
+        |> Check.QuickThrowOnFailure
+
+    [<Test>]
+    let ``dealerStrategyHitOnSoft17はScore(18)以上の場合Standすること`` () =
+        Prop.forAll (getScoreGen true 18 21 |> Arb.fromGen) (Game.dealerStrategyHitOnSoft17 >> (=) Game.DealerAction.Stand)
+        |> Check.QuickThrowOnFailure
+
+    [<Test>]
+    let ``dealerStrategyHitOnSoft17はHard17の場合Standすること`` () =
+        Game.dealerStrategyHitOnSoft17 (Score 17, false) |> should equal Game.DealerAction.Stand
+
+    [<Test>]
+    let ``dealerStrategyHitOnSoft17はSoft17の場合Standすること`` () =
+        Game.dealerStrategyHitOnSoft17 (Score 17, true) |> should equal Game.DealerAction.Hit
+
+    [<Test>]
+    let ``dealerStrategyHitOnSoft17はBustedの場合例外が発生すること`` () =
+        fun () -> Game.dealerStrategyHitOnSoft17 (Busted, false) |> ignore
+        |> should throw typeof<exn>
+        fun () -> Game.dealerStrategyHitOnSoft17 (Busted, true) |> ignore
+        |> should throw typeof<exn>
+
+    [<Test>]
+    let ``validateRuleにPayRatioWinningBlackjack<1を渡すとFailureを返すこと`` () =
+        let ruleGen = gen {
+            let! rule = Arb.generate<GameRule>
+            let! payRatio = Arb.generate<float> |> Gen.suchThat ((>) 1.0)
+            return { rule with PayRatioWinningBlackjack = payRatio }
+        }
+        Prop.forAll (Arb.fromGen ruleGen) (Game.validateRule >> Result.isFailure)
+        |> Check.QuickThrowOnFailure
+
+    [<Test>]
+    let ``validateRuleにPenetration<0を渡すとFailureを返すこと`` () =
+        let ruleGen = gen {
+            let! rule = Arb.generate<GameRule>
+            let! penetration = Arb.generate<float> |> Gen.suchThat ((>) 0.0)
+            return { rule with Penetration = penetration }
+        }
+        Prop.forAll (Arb.fromGen ruleGen) (Game.validateRule >> Result.isFailure)
+        |> Check.QuickThrowOnFailure
+
+    [<Test>]
+    let ``validateRuleにPenetration>1渡すとFailureを返すこと`` () =
+        let ruleGen = gen {
+            let! rule = Arb.generate<GameRule>
+            let! penetration = Arb.generate<float> |> Gen.suchThat ((<) 1.0)
+            return { rule with Penetration = penetration }
+        }
+        Prop.forAll (Arb.fromGen ruleGen) (Game.validateRule >> Result.isFailure)
+        |> Check.QuickThrowOnFailure
